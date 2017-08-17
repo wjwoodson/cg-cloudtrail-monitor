d99ba427baf6605fb7289f68aef6e790f7aac66c
Will Woodson <william.woodson@gsa.gov>
2017-08-17 16:48:53 -0500

    Drop verbose flag, add --days,-d to specify event window

--- get-cloudtrail.sh
+++ get-cloudtrail.sh
@@ -2,10 +2,41 @@
 
 set -e 
 
-if [[ -z $ci_env ]]
+if [[ -n $1 && $1 =~ (-h|--help)$ ]]
 then
-  echo "Please set ENV \$ci_env"
-  exit 99
+  echo -e "
+  ./get-cloudtrail.sh [--help, -h] [--days -d]
+
+  Get cloudtrail events listed in https://cloud.gov/docs/ops/maintenance-list/#review-aws-cloudtrail-events
+
+  --help, -h        show this message
+  
+  --days, -d        number of days (now - days) to get events                    
+  "
+  exit
+fi
+
+#use AWS_PROFILE if it is set
+profile="default"
+if [[ ! -z $AWS_PROFILE ]]
+then
+  profile=$AWS_PROFILE
+fi
+
+echo "======== ======== get-cloudtrail.sh ======== ========"
+echo "Getting events using profile: ${PROFILE}"
+
+# end event window at now
+end_ts=$(date +%s)
+
+if [[ -n $1 && $1 =~ (-d|--days)$ ]]
+then
+  start_ts=$(($end_ts - ($2 * 86400)))
+  echo "Days of events: ${2}"
+else
+  #start event window at (now - 1 day)
+  start_ts=$(($end_ts - 86400))
+  echo "Days of events: 1"
 fi
 
 #the types of events we need to lookup
@@ -24,28 +55,17 @@ lookupEvents=(
   "UpdateTrail"
 )
 
-# end window at now
-current_ts=$(date +%s)
-# start window at (now - 48 hours)
-start_ts=$(($current_ts - 172800))
-
+#display cloudtrail events for each EventName
 for name in ${lookupEvents[@]}
 do
   echo "======== ======== ${name} ======== ========"
   logs=$(
-    aws cloudtrail lookup-events --start-time $start_ts --end-time $current_ts \
+    aws cloudtrail lookup-events --start-time $start_ts --end-time $end_ts \
       --lookup-attributes AttributeKey=EventName,AttributeValue=$name \
-      --profile=$ci_env
+      --profile $profile
   )
-  if [[ -n $1 && $1 =~ (-v|--verbose)$ ]]
-  then
-    echo $logs | jq -r ' .Events[] | (.EventTime | todate) + " " + .EventName + " " + .Username + " " + 
-    (.CloudTrailEvent|fromjson| .sourceIPAddress + " " + .userAgent) + " \n" +
-    .CloudTrailEvent + " \n"'
-  else
-    echo $logs | jq -r ' .Events[] | (.EventTime | todate) + " " + .EventName + " " + .Username + " " + 
-    (.CloudTrailEvent|fromjson| .sourceIPAddress + " " + .userAgent)'
-  fi
+  echo $logs | jq -r ' .Events[] | (.EventTime | todate) + " " + .EventName + " " + .Username + " " + 
+  (.CloudTrailEvent|fromjson| .sourceIPAddress + " " + .userAgent)'
 done
 
-echo "Done looking up events"
+echo "Done looking up events."
